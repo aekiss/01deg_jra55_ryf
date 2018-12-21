2d38b9fdebb3da5d1b04a894fcce43fd169d4957
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2018-12-21 11:46:13 +1100

    Switch to minimal config based on f16a56b; disabled 3D daily output of uhrho_et, vhrho_nt, temp, salt and rho_dzt, south of 55S

--- sync_output_to_gdata.sh
+++ sync_output_to_gdata.sh
@@ -9,8 +9,10 @@
 # Set this directory to something in /g/data3/hh5/tmp/cosima/
 # Make a unique path for your set of runs.
 # DOUBLE-CHECK IT IS UNIQUE SO YOU DON'T OVERWRITE EXISTING OUTPUT!
-GDATADIR=/g/data3/hh5/tmp/cosima/access-om2-01/01deg_jra55v13_ryf8485_spinup6
+GDATADIR=/g/data3/hh5/tmp/cosima/access-om2-01/01deg_jra55v13_ryf8485_spinup6_newtopog
 
 mkdir -p ${GDATADIR}
 cd archive
 rsync --exclude "*.nc.*" --exclude "ocean_daily_3d_*" --exclude "*ocean_*_3hourly*" --exclude "*iceh_03h*" -vrltoD --safe-links output* ${GDATADIR}
+rsync -vrltoD --safe-links pbs_logs ${GDATADIR}
+
